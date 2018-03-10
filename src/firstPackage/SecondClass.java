723f55ac6afc1c073c55ff9814e0e7812c0bf6f8
ilyassir <iliyak6901@brandeis.edu>
2018-03-10 12:21:02 -0500

    added print method

--- src/firstPackage/SecondClass.java
+++ src/firstPackage/SecondClass.java
@@ -3,6 +3,9 @@ package firstPackage;
 public class SecondClass {
 public static void main(String[] args) {
 	System.out.println("good evening");
+	
 }
-
+public void print() {
+	System.out.println("hi new method");
+}
 }
