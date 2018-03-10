5f1b61725ba7c6d2c9c564bb99254b8878eee492
ilyassir <iliyak6901@brandeis.edu>
2018-03-10 12:26:27 -0500

    added two more print lines

--- src/firstPackage/FirstClass.java
+++ src/firstPackage/FirstClass.java
@@ -4,5 +4,7 @@ public class FirstClass {
 
 	public static void main(String[] args) {
 		System.out.println("good morning");
+	System.out.println("Second line");
+	System.out.println("Second line");
 	}
 }
